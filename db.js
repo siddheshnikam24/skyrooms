5392709ae1fa1f3b615741eb405ae1c931b3234e
siddheshnikam24 <84913782+siddheshnikam24@users.noreply.github.com>
2021-09-30 12:28:56 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-var mongoDBURL = 'mongodb+srv://sujeet:sujeet123@cluster0.tsmr5.mongodb.net/skyrooms'
+var mongoDBURL = 'mongodb+srv://siddhesh:siddhesh123@cluster0.69h5q.mongodb.net/skyrooms'
 
 
-mongoose.connect(mongoDBURL)
+mongoose.connect(mongoDBURL);
 
 var dbconnect = mongoose.connection
 
 dbconnect.on('error', () => {
-    console.log('MongoDB connection failed.')
+    console.log('MongoDB connection failed.');
 })
 
 dbconnect.on('connected', () => {
-    console.log('MongoDB connected successfully.')
+    console.log('MongoDB connected successfully.');
 })
 
-module.exports = mongoose
+module.exports = mongoose;
